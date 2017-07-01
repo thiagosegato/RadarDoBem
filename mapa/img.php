90ab3815f301c506ef0f07bc1cb37af666bac523
nicolaubrasil <nicolaubrasil@gmail.com>
2017-07-01 11:48:00 -0300

    change content type for jpeg

--- mapa/img.php
+++ mapa/img.php
@@ -18,7 +18,7 @@ $query = query($sql);
 if($query->rowCount() > 0){
 	
 	$file = $query->fetch();	
-	header('Content-Type: image/jpg');
+	header('Content-Type: image/jpeg');
 	header('Content-Disposition: inline; filename="'.$hash.'"');
 	header('Pragma: no-cache');
 	readfile($file['ds_path'].$file['ci_foto'].($thumb ? '_thumb' : '').'.jpg');	
